3ff786e614b8ef60b7da3db22d48409f670f7490
tobeypeters <tobeypeters@hotmail.com>
2021-01-27 14:18:13 -0500

    Updated new_install.sh

--- new_install.sh
+++ new_install.sh
@@ -79,8 +79,11 @@ sudo apt install -y cava
 sudo add-apt-repository ppa:tiheum/equinox
 sudo apt-get update && sudo apt-get install faenza-icon-theme
 
-apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
-sudo apt install libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+sudo apt install -y build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python3-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev
+sudo apt install -y libxcb-xkb-dev libxcb-xrm-dev libxcb-cursor-dev libasound2-dev libpulse-dev i3-wm libjsoncpp-dev libmpdclient-dev libcurl4-openssl-dev libnl-genl-3-dev
+
+#In case, krunner is running
+# sudo apt install plasma-workspace
 
 # enable firewall
 # setup timeshift
