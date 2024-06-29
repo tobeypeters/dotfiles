e40d6d5175e92f53bdafa9fd9a9656169b92fe5c
tobeypeters <tobeypeters@hotmail.com>
2024-06-29 00:31:49 -0400

    Updated cycle.py

--- .config/polybar/scripts/cycle.py
+++ .config/polybar/scripts/cycle.py
@@ -51,8 +51,9 @@ args: arg_namespace = parser.parse_args()
 i3: i3_con = i3_con.Connection()
 
 """import logging
-logging.basicConfig(filename='app.log', level=logging.INFO)
-with open('app.log', 'w'):
+logfile='/home/tibegato/app.log'
+logging.basicConfig(filename=logfile, level=logging.INFO)
+with open(logfile, 'w'):
     pass
 for w in windows:
     logging.info(w)
@@ -61,35 +62,19 @@ for w in windows:
 windows: list[int] = []
 switcher: dict[int, str] = {}
 
-def fill_nodes(f: Union[i3_con.Connection, str], e: i3_events.WindowEvent = None) -> None:
+def fill_nodes(f: str) -> None:
     def filler_up(tree: Optional[dict[str, Any]] = None):
-        if tree is None:
-            tree = loads(output_check(['i3-msg', '-t', 'get_tree']).decode('utf8'))
-
-        if tree['window_type'] == 'normal':
-            return {} if args.onlyclass and (
-                        not 'class' in tree['window_properties'] or
-                        not tree['window_properties']['class'] ==
-                        args.onlyclass[0]
-                        ) else [tree]
-
-        node_buffer = (
-                        [n['window'], n['name'], n['focused']]
-                        for nodes in ['nodes', 'floating_nodes']
-                        for node in tree[nodes]
-                        for n in filler_up(node)
-                      )
+        node_buffer = [l for ws in i3.get_tree().workspaces()
+                       for l in ws if l.type == 'con' and
+                       (not args.onlyclass or l.window_class == args.onlyclass[0])
+                      ]
 
         for n in node_buffer:
             if f == 'switcher':
-                if not n[2]:
-                    switcher[cast(int, n[0])] = cast(str, n[1])
+                switcher[n.window] = n.name
             else:
-                if not n[0] in windows:
-                    windows.append(cast(int, n[0]))
-
-                if n[2]:
-                    globals()['focusedID'] = cast(int, n[0])
+                if not n.window in windows:
+                    windows.append(n.window)
 
         return {}
 
@@ -98,20 +83,25 @@ def fill_nodes(f: Union[i3_con.Connection, str], e: i3_events.WindowEvent = None
 def switch_window(c: i3_con.Connection, e: i3_events.BindingEvent) -> None:
     wc: int = len(windows)
 
+#    process_exec(f'spd-say {wc}')
+
     if wc > 0:
+        focusedID: int = i3.get_tree().find_focused().window or windows[0]
+
         binding_cmd: str = e.ipc_data['binding']['command']
         if binding_cmd in commands:
             if binding_cmd in [commands[0], commands[1]]:
                 # We're tabbing forward or backward
+
                 focus_idx: int = (
                                     (windows.index(focusedID) if focusedID > 0 else 0) +
                                     (1 if (binding_cmd == commands[0]) else -1)
-                                    ) % wc
+                                 ) % wc
 
                 process_exec(f'i3-msg [id={windows[focus_idx]}] focus') # focus it using i3-msg
 
             if binding_cmd == commands[2]:
-                fill_nodes('switcher', None)
+                fill_nodes('switcher')
 
                 if switcher:
                     # Is the program a terminal?  If so, what's running in it?
@@ -201,29 +191,32 @@ def container_focused() -> bool:
     # Make sure a app window "con" is focused and not the desktop "workspace".
     return i3.get_tree().find_focused().type == 'con'
 
-def close_focus_window(c: i3_con.Connection, e: i3_events.WindowEvent) -> None:
-    if e.container.window in windows:
-        if e.ipc_data['change'] == 'close':
-            windows.remove(e.container.window)
-            globals()['focusedID'] = 0
-        elif swapID[0] and container_focused():
-            process_exec(f"i3-msg swap container with id {swapID[0]}") # Swap the containers
-            process_exec(f"i3-msg [id={swapID[0]}] focus") # focus the container
-            marker(prefix='un')
-        else:
-            globals()['focusedID'] = e.container.window
+def new_window(c: i3_con.Connection, e: i3_events.WindowEvent) -> None:
+    windows.append(e.container.window)
+#    process_exec(f'echo new_window: {windows} >> ~/cycle.txt')
+
+def close_window(c: i3_con.Connection, e: i3_events.WindowEvent) -> None:
+    windows.remove(e.container.window)
+
+def focus_window(c: i3_con.Connection, e: i3_events.WindowEvent) -> None:
+    if swapID[0] and container_focused():
+        process_exec(f"i3-msg swap container with id {swapID[0]}") # Swap the containers
+        process_exec(f"i3-msg [id={swapID[0]}] focus") # focus the container
+        marker(prefix='un')
 
 commands = ['nop window next', 'nop window prev', 'nop window selector', 'nop window swap']
 
-focusedID: int = 0
-
 swapID: list[int] = [0]
+
+#process_exec(f'echo START: \n> ~/cycle.txt')
 
 fill_nodes('')
 
-i3.on('window::new', fill_nodes)
-i3.on('window::close', close_focus_window)
-i3.on('window::focus', close_focus_window)
+#process_exec(f'spd-say {len(windows)}')
+
+i3.on('window::new', new_window)
+i3.on('window::close', close_window)
+i3.on('window::focus', focus_window)
 i3.on('binding', switch_window)
 
 i3.main()
